1875ec782c1cc87f9ce5acf5469dddc19cc1fed2
alina <khomkolovaalina@gmail.com>
2024-10-16 17:26:55 +0200

    создала новое приложение и описала модели

---
+++ school/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+from users.common import NULLABLE
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название')
+    image = models.FileField(verbose_name='Превью', **NULLABLE)
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+        ordering = ('title',)
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, related_name='lesson', on_delete=models.CASCADE)
+    title = models.CharField(max_length=150, verbose_name='Название')
+    image = models.FileField(verbose_name='Превью', **NULLABLE)
+    description = models.TextField(verbose_name='Описание')
+    url = models.URLField(**NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+        ordering = ('title',)
